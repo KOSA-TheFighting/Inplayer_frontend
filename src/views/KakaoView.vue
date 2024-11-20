9b8e11158626d844510691085f713cdb6e2ff340
docodack <docodack@naver.com>
2024-11-20 19:07:03 +0900

    Commit: 소셜 로그인 수정

--- src/views/KakaoView.vue
+++ src/views/KakaoView.vue
@@ -1,29 +1,21 @@
 <script setup>
-import axios from 'axios'
 import { useRoute } from 'vue-router'
 import { api } from '@/api/requestAPI.js'
 
+const currentRoute = useRoute()
+const code = currentRoute.query.code
 
-const currentRoute = useRoute();
-const code = currentRoute.query.code;
-console.log("currentRoute = " , currentRoute);
-console.log("code = " , code);
-
-alert("code1: " + code)
-
-const getKakaoToken = async (code) => {
+const getKakaoToken = async code => {
+  const result = await api.get('/kakao/oauth/callback', { code: code })
   alert(code)
-    const result = await api.get('/kakao/oauth/callback', {code:code});
-    return result;
-  };
+  return result
+}
 
 getKakaoToken(code)
 </script>
 
 <template>
-
+  <div>카카오 계정으로 로그인했습니다</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,49 +1,12 @@
 <script setup>
-
-window.Kakao.init("8327d68228a934423bed1bd4436581fd");
-
-// export default {
-//   name: "LoginKakao",
-//   methods: {
-//     loginWithKakao() {
-//       const params = {
-//         redirectUri: "http://localhost:8280/oauth/kakao",
-//       };
-//       window.Kakao.Auth.authorize(params);
-//     },
-//   },
-// };
-
-
+window.Kakao.init('8327d68228a934423bed1bd4436581fd')
 
 const handleKakaoLogin = () => {
-
   const params = {
-    redirectUri: "http://localhost:5173/oauth/kakao",
-  };
-  window.Kakao.Auth.authorize(params);
+    redirectUri: 'http://localhost:5173/oauth/kakao',
+  }
+  window.Kakao.Auth.authorize(params)
 }
-//
-// const getKakaoAccount = ()  =>  async {
-//   window.Kakao.API.request({
-//     url: "/v2/user/me",
-//     success: (res) => {
-//       const kakao_account = res.kakao_account;
-//       const ninkname = kakao_account.profile.ninkname;
-//       const email = kakao_account.email;
-//       console.log("ninkname", ninkname);
-//       console.log("email", email);
-//
-//       //로그인처리구현
-//
-//       alert("로그인 성공!");
-//     },
-//     fail: (error) => {
-//       console.log(error);
-//     },
-//   });
-// },
-
 </script>
 
 <template>
